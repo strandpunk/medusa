6a035e4ce7601ae71909fee043d5659866f94375
Anatoliy <strandpunk@yahoo.com>
2023-07-03 13:35:49 +0300

    canvas update

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ function App() {
 
 
   return (
+    <div style={{ width: "100vw", height: "100vh" }}>
     <Canvas camera={{
       fov: 64,
       position: [2, 2, -2]
     }}>
       <Experience />
     </Canvas>
+    </div>
   )
 }
 
